40fd8df466cf68eba44cf2af633052ad5b5985e4
Dennis <dennisbircan@gmail.com>
2024-08-28 15:30:32 +0200

    Uppgift loop9

---
+++ Loop8/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Mata in ett tal: ");
+bool success = false;
+
+do
+{
+    
+    success = int.TryParse(Console.ReadLine(), out int tal);
+
+    if (tal >= 30 || success == false)
+    {
+        Console.Write("Du har matat in fel, försök igen: ");
+        success = false;
+    }
+    else
+    {
+        for (int i = tal +1; i <= 30; i++)
+        {
+            
+            Console.WriteLine(i);
+        }
+    }
+}
+while (success != true);
+
+Console.ReadKey();
---
+++ Loop9/Program.cs
@@ -0,0 +1,35 @@
+﻿string userName = "";
+string userAddress = "";
+string userPostalCode = "";
+string userCity = "";
+do
+{
+    if (userName == "")
+    {
+        Console.Write("Ange namn: ");
+        userName = Console.ReadLine();
+    }
+    if (userAddress == "")
+    {
+        Console.Write("Ange adress: ");
+        userAddress = Console.ReadLine();
+    }
+    if (userPostalCode == "") 
+    {
+        Console.Write("Ange postkod: ");
+        userPostalCode = Console.ReadLine();
+    }
+    if (userCity == "")
+    {
+        Console.Write("Ange ort: ");
+        userCity = Console.ReadLine();
+    }
+    if(userName != "" && userAddress != "" && userPostalCode != "" && userCity != "")
+        break;
+    
+}
+while (true);
+
+Console.WriteLine($"{userName}\n{userAddress}\n{userPostalCode}\n{userCity}");
+
+Console.ReadKey();
