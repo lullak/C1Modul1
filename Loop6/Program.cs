044f1c1c2f8a0ed6e669f01e4f78a9c23fdb2ef1
Dennis <dennisbircan@gmail.com>
2024-08-28 14:36:14 +0200

    Loop 6

---
+++ Loop6/Program.cs
@@ -0,0 +1,17 @@
+﻿int tal = 0;
+
+do
+{
+    Console.Write("Mata in ett tal: ");
+    tal = Convert.ToInt32(Console.ReadLine());
+
+    if (tal < 10)
+        Console.WriteLine("Värdet för lågt!");
+    else if (tal > 10)
+        Console.WriteLine("Värdet är fär högt!");
+}
+while (tal != 10);
+
+Console.WriteLine("Du matade in rätt tal!");
+
+Console.ReadKey();
