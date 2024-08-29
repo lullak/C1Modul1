db2f47ad8b6fd92823b7b3b6fd2d24f6862c757d
lullak <dennisbircan@gmail.com>
2024-08-29 10:16:25 +0200

    Gissa tal övning lektion

---
+++ GissaTal/Program.cs
@@ -0,0 +1,26 @@
+﻿Random random = new Random();
+int randomNumber = random.Next(1,21
+    );
+int guessedNumber;
+
+
+while(true){
+    Console.WriteLine("Gissa tal, mellan 1 och 20: ");
+    guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (guessedNumber < randomNumber)
+    {
+        Console.WriteLine("Du gissade för lågt");
+    }
+    else if (guessedNumber > randomNumber)
+    {
+        Console.WriteLine("Du gissade för högt");
+    }
+    else {
+        Console.WriteLine("Du gissade rätt!");
+        break;
+    }
+}
+
+Console.WriteLine(randomNumber);
+Console.ReadKey();
