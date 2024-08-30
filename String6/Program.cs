814beaeacd3de540939b900a0d4e9852f45e77ff
Dennis <dennisbircan@gmail.com>
2024-08-30 13:21:16 +0200

    String 6

---
+++ String6/Program.cs
@@ -0,0 +1,17 @@
+﻿//Du har en kommaseparerad sträng som ser ut så här ”ett, två, tre, fyra, fem, sex, sju”. Skapa en 
+//array där varje värde i strängen hamnar i en egen position i arrayen dvs ett hamnar i första 
+//positionen och två i andra positionen och tre i den tredje osv. 
+
+string stringToArray = "ett,två,tre,fyra,fem,sex,sju";
+
+string[] newStringArray = stringToArray.Split(',');
+
+foreach (string str in newStringArray)
+{
+    Console.WriteLine(str);
+}
+
+
+
+
+Console.ReadKey();
