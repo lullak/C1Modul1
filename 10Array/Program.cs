631fd107bc9606f955b5f845ba9e9c53d8cf719a
Dennis <dennisbircan@gmail.com>
2024-08-30 10:15:55 +0200

    array 10 klar

--- 10Array/Program.cs
+++ 10Array/Program.cs
@@ -7,6 +7,17 @@ string[] words = new string[5];
 
 for (int i = 0; i < words.Length; i++)
 {
+    Console.Write("Mata in ett ord: ");
     words[i] = Console.ReadLine();
 }
 
+string longWord = "";
+foreach (string word in words)
+{
+    if (longWord.Length < word.Length) {
+        longWord = word;
+    }
+}
+
+Console.WriteLine(longWord);
+Console.ReadKey();
