d7d092b18925d0b0d99740ae8ce669ff7659f69f
Dennis <dennisbircan@gmail.com>
2024-08-30 14:33:29 +0200

    String 8

---
+++ String 7/Program.cs
@@ -0,0 +1,26 @@
+﻿//Be användaren mata in en mailadress. Programmet skall kontrollera att inmatningen är rätt 
+//dvs att det finns ett @ tecken och att det finns en . med 2 eller 3 tecken efter. Meddela 
+//användaren om det är rätt eller felaktig adress 
+
+
+Console.Write("Mata in din mejladress: ");
+string mailAddress = Console.ReadLine();
+int mailAddressLength = mailAddress.Length;
+
+if (mailAddress.Contains("@"))
+{
+    if (mailAddress.LastIndexOf(".") == mailAddressLength - 3 || mailAddress.LastIndexOf(".") == mailAddressLength - 4)
+    {
+        Console.WriteLine("Korrekt mejladress");
+    }
+    else 
+    {
+        Console.WriteLine("Felaktig mejladress");
+    }
+}
+else
+{
+    Console.WriteLine("Felaktig mejladress");
+}
+
+Console.ReadLine();
---
+++ String8/Program.cs
@@ -0,0 +1,11 @@
+﻿//Sortera strängen "231596874" till strängen "123456789" med en egen sortfunktion
+
+// Sort via Array.Sort
+//string numbers = "231596874";
+//char[] numbersArray = numbers.ToCharArray();
+
+//Array.Sort(numbersArray);
+//string sortedArray = new string(numbersArray);
+//Console.WriteLine(sortedArray);
+
+Console.ReadLine();
